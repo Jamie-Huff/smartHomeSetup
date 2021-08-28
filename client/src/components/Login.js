8df02a3421cbf714fc417f335f36f3202b97cac1
mohammedpythons <mohammedshokai99@gmail.com>
2021-08-28 10:03:55 -0400

    add login logic frontend

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,29 @@
 import React, { useState } from "react";
 import "./Login.scss";
+import axios from 'axios'
 
 export default function Login (props) {
+	const [data, setData] = useState({
+		email: "",
+		password: ""
+	})
+
+
+
+	const handleSubmit = (e) => {
+		console.log(data)
+		e.preventDefault();
+		return axios.post("http://localhost:3002/signup", data)
+	}
 	return (
-		<div>Login</div>
+		<div>
+			<form>
+			<label>Email</label>
+			<input type="text" placeholder="email@example.com" onChange={(e) => setData({...data, email: e.target.value})}/>
+			<label>password</label>
+			<input type="password" onChange={(e) => setData({...data, password: e.target.value})} />
+			<button onClick={handleSubmit}>Sign Up</button>
+			</form>
+		</div>
 	)
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
 import "./Signup.scss";
+import axios from 'axios'
 
 export default function Signup (props) {
+	const [data, setData] = useState({
+		name: "",
+		email: "",
+		password: ""
+	})
+
+
+
+	const handleSubmit = (e) => {
+		console.log(data)
+		e.preventDefault();
+		return axios.post("http://localhost:3002/signup", data)
+	}
 	return (
-		<div>Signup</div>
+		<div>
+			<form>
+				<label>User name</label>
+				<input type="text" onChange={(e) => setData({...data, name: e.target.value})} />
+				<label>Email</label>
+				<input type="text" placeholder="email@example.com" onChange={(e) => setData({...data, email: e.target.value})}/>
+				<label>password</label>
+				<input type="password" onChange={(e) => setData({...data, password: e.target.value})} />
+				<button onClick={handleSubmit}>Sign Up</button>
+			</form>
+		</div>
 	)
 }
+
