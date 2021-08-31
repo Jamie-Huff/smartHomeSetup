6f18cf9861eeae82fa0ca3a334dcf4ab3b1eccf2
Jamie <jamiehuff737@gmail.com>
2021-08-31 18:27:17 -0400

    added sql queries dependant on data from front end

--- node-API/helpers/survey.js
+++ node-API/helpers/survey.js
@@ -1,164 +1,45 @@
-require('dotenv').config()
-const { Pool } = require('pg');
-const dbParams = require('../lib/db');
-const db = new Pool(dbParams);
-db.connect()
-  .catch(err => {
-    console.error(err)
-  })
-
-
-// let users = ''
-// because this function is async it wont give us our data back properly need to have it all in a callback
-const userListGenerator = (db) => {
-  return db.query('SELECT * FROM PRODUCTS;')
-    .then(data => {
-      const users = data.rows
-      return users
-    })
-}
-
-
-
-
-
-
-// const logUser = async () => {
-//   return userListGenerator(db)
-//     .then(result => {
-//       // console.log('@@@', result)
-//       const sorted = sortCategory(result)
-//       return sorted
-//     })
-// }
-
-// const log2 = async () => {
-//   try {
-//     const result = await db.query('SELECT * FROM USERS WHERE id = 1;')
-//     // console.log('#####', result.rows)
-//     return result.rows
-//   } catch (error) {
-//     console.log('err@@', error)
-//   }
-// }
-
-const data = {
-  budget: 500,
-  categories: {
-    // products have a default quantity of 1
-    5: {
-      id: 5,
-      name: 'interior cameras',
-      quantity: 5
-    },
-    1: {
-      id: 1,
-      name: 'hubs',
-      quantity: 1
-    },
-    7: {
-      id: 7,
-      name: 'vaccumes',
-      quantity: 1
+const categoryFinder = (query) => {
+  for (const category in query.categories) {
+    if (category === 'lights') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'interior lights', id: 2}, 
+        {quantity: query.categories[category].quantity, name: 'yard lights', id: 3})
     }
-  },
-  rooms: ['common area', 'inspecific']
-}
-
-const products = {
-  1: {
-    id: 1,
-    name: 'Google Nest Hub Max',
-    description: 'short desc',
-    image: 'img link',
-    price: 299.00,
-    category_id: 1,	
-    room_id: 1
-  },
-  2: {
-    id: 13,
-    name: 'Kyvol Cybovac E30 Robot Vacuum Cleaner',
-    description: 'short desc',
-    image: 'img link',
-    price: 189.99,
-    category_id: 7,	
-    room_id: 1
-    },
-  3: {
-    id: 4,
-    name: 'camera',
-    description: 'short desc',
-    image: 'img link',
-    price: 189.99,
-    category_id: 8,	
-    room_id: 1
-    },
-  4: {
-    id: 70,
-    name: 'vaccume 2',
-    description: 'short desc',
-    image: 'img link',
-    price: 189.99,
-    category_id: 7,	
-    room_id: 1
-    },
-}
-
-// the purpose of generate survey resutls, is too generate a formatted version of the products
-// that are suggested for the users home
-// depending on what answers they give, will effect the products suggested
-
-
-// using our categories, filters them down into an array
-const categoryArray = function(catagories) {
-  let array = []
-  for (const category in catagories) {
-    array.push(Number(category))
-  }
-  return array
-}
-
-// possible products will filter through a database query of the products,
-// using the catagories from data passed from the front end
-// we will return a new array of objects containing our possible products regardless of other factors
-const possibleProducts = function (categories) {
-  // this value contains the array of categories of products we are looking for
-  let categoryAllowed = categoryArray(categories)
-  // empty final object of reccomended products
-  let productsByCatagories = []
-  for (const product in products) {
-    if (categoryAllowed.includes(products[product].category_id)) {
-      productsByCatagories.push(products[product]);
+    if (category === 'speakers') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'speakers', id: 16})
     }
-  }
-  // need to go through our fake products and select only the ones whos catagory matches
-  return productsByCatagories
-}
-let filteredProducts = possibleProducts(data.categories, products)
-
-const generateProductsInCategory = function (products, categories) {
-  // sets categoriesArray equal to an array version of the categories a customer is looking for
-  const categoriesArray = categoryArray(categories)
-  // categoryProductGrouping is equal to an object which will contain our products related to each category
-  let categoryProductGrouping = {}
-  for (const category of categoriesArray) {
-    categoryProductGrouping[category]= []
-  }
-  for (const product in products) {
-    if (categoriesArray.includes(products[product].category_id)) {
-      categoryProductGrouping[products[product].category_id].push(products[product])
+    if (category === 'hubs') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'hubs', id: 1})
     }
+    if (category === 'appliances') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'vaccumes', id: 7},
+        {quantity: query.categories[category].quantity, name: 'refrigerators', id: 8},
+        {quantity: query.categories[category].quantity, name: 'stoves', id: 9},
+        {quantity: query.categories[category].quantity, name: 'dishwashers', id: 10},
+        {quantity: query.categories[category].quantity, name: 'washers', id: 11},
+        {quantity: query.categories[category].quantity, name: 'dryers', id: 12},
+        {quantity: query.categories[category].quantity, name: 'coffee makers', id: 14},
+        {quantity: query.categories[category].quantity, name: 'televisions', id: 17})
+    }
+    if (category === 'thermostat') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'thermostats', id: 13})
+    }
+    if (category === 'security') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'yard cameras', id: 4},
+        {quantity: query.categories[category].quantity, name: 'interior camears', id: 5},
+        {quantity: query.categories[category].quantity, name: 'door locks', id: 15},
+        {quantity: query.categories[category].quantity, name: 'door bells', id: 6})
+    }
+    if (category === 'garage') {
+      categories.push(
+        {quantity: query.categories[category].quantity, name: 'garage door opener', id: 18})
+    } 
   }
-  return categoryProductGrouping
 }
 
-// filter through the products and add the products to local hold depending on the lowest val
-const reccomendedProductByPrice = function(products, budget, categories, room) {
-  let localHold = {
-    productIds: [],
-    budgetRemaining: budget
-  }
-  console.log(products)
-}
-//console.log(generateProductsInCategory(filteredProducts, data.categories))
-// console.log(reccomendedProductByPrice(generateProductsInCategory(filteredProducts, data.categories)))
+module.exports = categoryFinder
--- node-API/routes/surveyData.js
+++ node-API/routes/surveyData.js
@@ -1,25 +1,99 @@
 const express = require('express');
+const { db } = require('../server');
 const router = express.Router();
 
 
 const surveyData = (db) => {
 
-    router.post("/", (req, res) => {
-        //console.log("IN POST SURVEY", (req.body))
-        let query = req.body
-        let categories = query.categories
-        console.log('Our categories: ', categories)
-        console.log('Our budget: ', query.budget)
-        console.log('Our rooms: ', query.rooms)
-        db.query(`SELECT * FROM rooms`)
-            .then(data => {
-                // for starters, using our rooms, we need to narrow down our products for just those that have that room id
-                console.log('rooms that match', data.rows)
-            })
+  router.post("/", (req, res) => {
+    //console.log("IN POST SURVEY", (req.body))
+    let query = req.body
+    let roomQuery = []
+    let categoryQuery = []
+    let categories = []
+    for (const room of query.rooms) {
+      roomQuery.push(`rooms.name = '${room}'`)
+    }
+    // this produces a query of all products, regardless of category, it just filters by room id
+    // need to adjust this to filter by the category aswell.
+
+    // for examples this will give us all products for the common area,
+    // however, we only want products that match the common area, and are listed as one of our categories
+    // this for loops makes the variable categories help adjust our product results
+
+    for (const category in query.categories) {
+      if (category === 'lights') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'interior lights', id: 2}, 
+          {quantity: query.categories[category].quantity, name: 'yard lights', id: 3})
+      }
+      if (category === 'speakers') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'speakers', id: 16})
+      }
+      if (category === 'hubs') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'hubs', id: 1})
+      }
+      if (category === 'appliances') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'vaccumes', id: 7},
+          {quantity: query.categories[category].quantity, name: 'refrigerators', id: 8},
+          {quantity: query.categories[category].quantity, name: 'stoves', id: 9},
+          {quantity: query.categories[category].quantity, name: 'dishwashers', id: 10},
+          {quantity: query.categories[category].quantity, name: 'washers', id: 11},
+          {quantity: query.categories[category].quantity, name: 'dryers', id: 12},
+          {quantity: query.categories[category].quantity, name: 'coffee makers', id: 14},
+          {quantity: query.categories[category].quantity, name: 'televisions', id: 17})
+      }
+      if (category === 'thermostat') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'thermostats', id: 13})
+      }
+      if (category === 'security') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'yard cameras', id: 4},
+          {quantity: query.categories[category].quantity, name: 'interior camears', id: 5},
+          {quantity: query.categories[category].quantity, name: 'door locks', id: 15},
+          {quantity: query.categories[category].quantity, name: 'door bells', id: 6})
+      }
+      if (category === 'garage') {
+        categories.push(
+          {quantity: query.categories[category].quantity, name: 'garage door opener', id: 18})
+      } 
+    }
+
+    for (const category in categories) {
+      categoryQuery.push(`categories.name = '${categories[category].name}'`)
+    }
+
+    // db.query(`SELECT DISTINCT categories.id FROM categories WHERE ${categoryQuery.join(' OR ')}`)
+    //   .then(data => {
+    //     console.log('category query', data)
+    //   }) 
+    console.log(query.rooms)
+    console.log(categories)
+
+    // get all products that match the room or category requestedg
+    db.query(`SELECT products.* 
+    FROM products 
+    WHERE products.room_id 
+    IN (SELECT DISTINCT rooms.id FROM rooms WHERE ${roomQuery.join(' OR ')}) 
+    AND products.category_id
+    IN (SELECT DISTINCT categories.id FROM categories WHERE ${categoryQuery.join(' OR ')})
+    ORDER BY products.price`)
+      .then(data => {
+        let filteredProducts = data.rows
+        console.log(filteredProducts)
+        res.json(filteredProducts)
+        })
+      .catch(err => {
+        console.log(err)
+      })
     })
 
 
-    return router;
+return router;
 }
 
 
