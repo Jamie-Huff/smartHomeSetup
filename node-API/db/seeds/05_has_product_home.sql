bdc5da00f5101d7dbe26ee794f2ce7334f91f885
Jamie <jamiehuff737@gmail.com>
2021-09-03 17:57:16 -0400

    add survey to table 5

--- node-API/db/seeds/05_has_product_home.sql
+++ node-API/db/seeds/05_has_product_home.sql
@@ -1,9 +1,7 @@
 -- HOME_PRODUCT DATA
 INSERT INTO has_product_home
-  (product_id, user_id)
+  (product_id, user_id, survey_id)
 VALUES
-  (1, 1),
-  (16, 1),
-  (30, 1),
-  (16, 2),
-  (7, 2);
+  (1, 1, 1),
+  (16, 1, 1),
+  (30, 1, 1);
