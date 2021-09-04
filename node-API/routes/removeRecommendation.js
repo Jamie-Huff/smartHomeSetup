54d0d991257876427a40f8b4b52946257820042e
Jamie <jamiehuff737@gmail.com>
2021-09-03 20:44:12 -0400

    added info to addProduct, DeleteProduct, remoteRec routes

--- node-API/routes/removeRecommendation.js
+++ node-API/routes/removeRecommendation.js
@@ -7,7 +7,26 @@ const generateRecommendations = require("../helpers/productRecommendations")
 
 const removeRecommendation = (db) => {
   router.post("/", async (req, res) => {
+    let query = req.body
+    let email = ''
 
+    jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
+      email = decoded.email
+    })
+    // find the user by their email
+    let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
+    )).rows[0].id
+
+    let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
+
+      // need the last survey in the database for that user, should be the most recent
+
+    let mostRecentSurvey = findSurvey[findSurvey.length - 1]
+
+    (await db.query(`DELETE FROM recommendations WHERE user_id = $2 AND product_id = $1 AND survey_id = $3`,
+      [query.product_id, userId, mostRecentSurvey.id]))
+
+      res.json('Product sucessfully remove from recommendations')
   })
 
   return router
