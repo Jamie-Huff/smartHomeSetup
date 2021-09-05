9efe8d0d38beb7819918ead7377063a5f823e7ad
Jamie <jamiehuff737@gmail.com>
2021-09-04 20:25:09 -0400

    complete add/del product home routes

--- node-API/routes/addProductHome.js
+++ node-API/routes/addProductHome.js
@@ -7,35 +7,37 @@ const generateRecommendations = require("../helpers/productRecommendations")
 
 const addProductHome = (db) => {
   router.post("/", async (req, res) => {
-    console.log("I GOT THE MESSAGE TO ADD PROD TO HOME", req.body)
+    let query = req.body
+    let email = ''
+    //given a user id, and product id
+      // res.body.user_id
+      // res.body.product_id
+    jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
+      email = decoded.email
+    })
+    // find the user by their email
+    let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
+    )).rows[0].id
 
-    // let query = req.body
-    // let email = ''
-    // //given a user id, and product id
-    //   // res.body.user_id
-    //   // res.body.product_id
-    // jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
-    //   email = decoded.email
-    // })
-    // // find the user by their email
-    // let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
-    // )).rows[0].id
+    console.log('email', email)
+    //need to find the users most recent survey, and add the product to it
+    console.log('userid', userId)
+    let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
 
+    // need the last survey in the database for that user, should be the most recent
 
-    // //need to find the users most recent survey, and add the product to it
+    let mostRecentSurvey = await findSurvey[findSurvey.length - 1]
+    console.log('most recent', mostRecentSurvey.id)
 
-    // let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
+    //Inside of the has_product_home table
+    //Add a row for the current product in the users home
 
-    // // need the last survey in the database for that user, should be the most recent
+    const addToHome = async (productId, userId, surveyId) => {
+      return (await db.query(`INSERT INTO has_product_home (product_id, user_id, survey_id) VALUES($1, $2, $3)`,
+      [productId, userId, surveyId]))
+    }
 
-    // let mostRecentSurvey = await findSurvey[findSurvey.length - 1]
-
-    // //Inside of the has_product_home table
-    // //Add a row for the current product in the users home
-
-    // (await db.query(`INSERT INTO has_product_home (product_id, user_id, survey_id) VALUES($1, $2, $3)`,
-    //   [query.product_id, userId, mostRecentSurvey.id]))
-
+    addToHome(query.product_id, userId, mostRecentSurvey.id)
       res.json('Product sucessfully added to home')
   })
 
--- node-API/routes/deleteProductHome.js
+++ node-API/routes/deleteProductHome.js
@@ -9,26 +9,30 @@ const deleteProductHome = (db) => {
   router.post("/", async (req, res) => {
 
     console.log("I GOT THE MESSAGE TO DELETE PROD FROM HOME", req.body)
-    // let query = req.body
-    // let email = ''
+    let query = req.body
+    let email = ''
 
-    // jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
-    //   email = decoded.email
-    // })
-    // // find the user by their email
-    // let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
-    // )).rows[0].id
-    // // remove a row from the has_product_home table 
-    //   // this means that a user removed that product from their home
+    jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
+      email = decoded.email
+    })
+    // find the user by their email
+    let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
+    )).rows[0].id
+    // remove a row from the has_product_home table 
+      // this means that a user removed that product from their home
 
-    // let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
+    let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
 
-    //   // need the last survey in the database for that user, should be the most recent
+      // need the last survey in the database for that user, should be the most recent
 
-    // let mostRecentSurvey = await findSurvey[findSurvey.length - 1]
+    let mostRecentSurvey = await findSurvey[findSurvey.length - 1]
 
-    // (await db.query(`DELETE FROM has_product_home WHERE user_id = $2 AND product_id = $1 AND survey_id = $3`,
-    //   [query.product_id, userId, mostRecentSurvey.id]))
+    const addToHome = async (productId, userId, surveyId) => {
+      return (await db.query(`DELETE FROM has_product_home WHERE user_id = $2 AND product_id = $1 AND survey_id = $3`,
+      [productId, userId, surveyId]))
+    }
+  
+      addToHome(query.product_id, userId, mostRecentSurvey.id)
 
       res.json('Product sucessfully remove from home')
   })
