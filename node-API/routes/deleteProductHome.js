379ededa1072a4cfb11fd7367643e812da5380bf
Jamie <jamiehuff737@gmail.com>
2021-09-05 13:45:46 -0400

    modularize code to import functions

--- node-API/routes/deleteProductHome.js
+++ node-API/routes/deleteProductHome.js
@@ -4,37 +4,23 @@ const router = express.Router();
 const categoryFinder = require('../helpers/survey')
 const jwt = require("jsonwebtoken");
 const generateRecommendations = require("../helpers/productRecommendations")
+const getUserFromToken = require('../helpers/getUserFromToken')
 
 const deleteProductHome = (db) => {
   router.post("/", async (req, res) => {
-
-    console.log("I GOT THE MESSAGE TO DELETE PROD FROM HOME", req.body)
     let query = req.body
-    let email = ''
-
-    jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
-      email = decoded.email
-    })
-    // find the user by their email
-    let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
-    )).rows[0].id
-    // remove a row from the has_product_home table 
-      // this means that a user removed that product from their home
-
+    let userId = await getUserFromToken(query.user, db)
     let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
-
-      // need the last survey in the database for that user, should be the most recent
-
     let mostRecentSurvey = await findSurvey[findSurvey.length - 1]
 
-    const addToHome = async (productId, userId, surveyId) => {
+    const deletefromHome = async (productId, userId, surveyId) => {
       return (await db.query(`DELETE FROM has_product_home WHERE user_id = $2 AND product_id = $1 AND survey_id = $3`,
       [productId, userId, surveyId]))
     }
   
-      addToHome(query.product_id, userId, mostRecentSurvey.id)
+    deletefromHome(query.product_id, userId, mostRecentSurvey.id)
 
-      res.json('Product sucessfully remove from home')
+    res.json('Product sucessfully remove from home')
   })
 
   return router
--- node-API/routes/removeRecommendation.js
+++ node-API/routes/removeRecommendation.js
@@ -4,28 +4,22 @@ const router = express.Router();
 const categoryFinder = require('../helpers/survey')
 const jwt = require("jsonwebtoken");
 const generateRecommendations = require("../helpers/productRecommendations")
+const roomObjMaker = require("../helpers/roomObjMaker")
+const roomArrayFinder = require("../helpers/roomArrayFinder")
+const compare = require("../helpers/objSorter")
+const getUserFromToken = require('../helpers/getUserFromToken')
 
 const removeRecommendation = (db) => {
   router.post("/", async (req, res) => {
     let query = req.body
-    console.log('@@@@', query)
-    let email = ''
-
-    jwt.verify(query.user.token, process.env.TOKEN, function(error, decoded) {
-      email = decoded.email
-    })
-    // find the user by their email
-    let userId = (await db.query(`SELECT * FROM users WHERE email = $1`, [email]
-    )).rows[0].id
-
+    let userId = await getUserFromToken(query.user, db)
     let findSurvey = (await db.query(`SELECT * FROM survey_results WHERE user_id = $1`, [userId])).rows
 
-      // need the last survey in the database for that user, should be the most recent
     let mostRecentSurvey = await findSurvey[findSurvey.length - 1]
-    // remove the recommendation
+
     await db.query(`DELETE FROM recommendations WHERE user_id = $2 AND product_id = $1 AND survey_id = $3`,
       [query.product_id, userId, mostRecentSurvey.id])
-    // get a list of the remaining recommendations
+
     let findProducts = (await db.query(`SELECT * FROM recommendations WHERE user_id = $1 AND survey_id = $2`, [userId, mostRecentSurvey.id])).rows
     let productIds = findProducts
     let productList = []
@@ -33,59 +27,12 @@ const removeRecommendation = (db) => {
       let productQuery = (await db.query(`SELECT * FROM products WHERE id = $1`, [product.product_id])).rows[0]
       productList.push(productQuery)
     }
-    // get the id's for each room
-    const roomArrayFinder = (products) => {
-      let roomIdArray = []
-      for (let product of products) {
-        if (!roomIdArray.includes(product.room_id)) {
-          roomIdArray.push(product.room_id)
-        }
-      }
-      return roomIdArray
-    }
 
     let roomsIds = roomArrayFinder(productList)
-
-    // using the id's for each room get a list of all the rooms
-
-    const roomObjMaker = async (rooms, products) => {
-      let roomsArray = []
-      for (const room of rooms) {
-        let roomObj = {name: null, id: room, cost: 0}
-        let roomDetails = (await db.query(`SELECT * FROM rooms WHERE id = $1`, [room])).rows[0]
-        roomObj.name = await roomDetails.name
-
-        for (const product of products) {
-          if (product.room_id === room) {
-            roomObj.cost += product.price
-          }
-        }
-        roomsArray.push(roomObj)
-      }
-      return roomsArray
-    }
-    
-    let roomsFinal = await roomObjMaker(roomsIds, productList)
-
-    function compare(a, b) {
-      // Use toUpperCase() to ignore character casing
-      const roomA = a.name.toUpperCase();
-      const roomB = b.name.toUpperCase();
-    
-      let comparison = 0;
-      if (roomA > roomB) {
-        comparison = 1;
-      } else if (roomA < roomB) {
-        comparison = -1;
-      }
-      return comparison;
-    }
+    let roomsFinal = await roomObjMaker(roomsIds, productList, db)
     
     roomsFinal.sort(compare);
-
-    console.log('@@@', roomsFinal)
-
-      res.json(roomsFinal)
+    res.json(roomsFinal)
   })
 
   return router
