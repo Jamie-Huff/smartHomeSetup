2ad37936069aed8976f74e84f1bc4ab80718d19e
mohammedpythons <mohammedshokai99@gmail.com>
2021-09-08 12:00:18 -0400

    fix styling

--- client/src/components/SmartVille.js
+++ client/src/components/SmartVille.js
@@ -15,12 +15,12 @@ export default function SmartVille(props) {
   } else {
     transitionNao(WELCOME)
   }
-  
+
   return (
     <div className="smartVille__top" style={{width:"100%"}}>
       <div className="smartVille__top smartVille__top-gettingStarted">
       </div>
-      <img src="images/nao_normal.png" alt="nao" style={{height:"400px", marginLeft: "440px", marginTop:"60px"}}/>
+      <img src="images/nao_normal.png" alt="nao" style={{height:"400px", marginLeft: "524px", marginTop:"80px"}}/>
     </div>
   )
 }
